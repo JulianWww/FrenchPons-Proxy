c51795871fcc85174ddd4c0e18cf38807f7675ea
Julian Wandhoven <Julian.wandhoven@gmail.com>
2023-04-17 14:45:24 +0200

    Update translator.php

--- translator.php
+++ translator.php
@@ -14,7 +14,7 @@
   $content = file_get_contents($url);
 
   if ($content == false) {
-    print("b<head><meta http-equiv=\"Refresh\" content=\"0; url='https://www.w3docs.com'\" /></head>");
+    print("b<head><meta http-equiv=\"Refresh\" content=\"0; url='https://github.com/JulianWww/FrenchPons-Proxy/blob/main/README.md'\" /></head>");
   }
 
   $content = str_replace("https://cdn.cookielaw.org/scripttemplates/otSDKStub.js", "", $content);
